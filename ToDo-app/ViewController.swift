163468c9a75467068a19cb786d7f0b978c512cb0
Bogdan Martinescu <liviu.bogdan.martinescu@gmail.com>
2017-05-02 22:38:49 +0300

    Initial Commit

---
+++ ToDo-app/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ToDo-app
+//
+//  Created by Bogdan Martinescu on 02/05/2017.
+//  Copyright © 2017 Bogdan Martinescu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
