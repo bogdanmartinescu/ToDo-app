8c81481bd6a5ee19c649b6d6bd1a06f35a30ca4c
Bogdan Martinescu <liviu.bogdan.martinescu@gmail.com>
2017-05-03 16:20:09 +0300

    populate table, add todos

--- ToDo-app/ViewController.swift
+++ ToDo-app/ViewController.swift
@@ -8,12 +8,34 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
 
+    @IBOutlet weak var textField: NSTextField!
+    @IBOutlet weak var importantCheckbox: NSButton!
+    @IBOutlet weak var tableView:NSTableView!
+    
+    var todoItems: [ToDoItem] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        getToDoItems()
         // Do any additional setup after loading the view.
+    }
+    
+    func getToDoItems() {
+        // get items from core data
+        // set them to class prop
+        // update the property
+        if let context = (NSApplication.shared().delegate as? AppDelegate)?.persistentContainer.viewContext {
+            do {
+                todoItems = try context.fetch(ToDoItem.fetchRequest())
+                print(todoItems.count)
+            } catch {
+                
+            }
+            // update table
+            tableView.reloadData()
+        }
     }
 
     override var representedObject: Any? {
@@ -22,6 +44,41 @@ class ViewController: NSViewController {
         }
     }
 
+    @IBAction func addClicked(_ sender: Any) {
+    
+        if textField.stringValue != "" {
+            if let context = (NSApplication.shared().delegate as? AppDelegate)?.persistentContainer.viewContext {
+                let todoItem = ToDoItem(context:context)
+                
+                todoItem.name = textField.stringValue
+                
+                if importantCheckbox.state == 0 {
+                    todoItem.important = false
+                } else {
+                    todoItem.important = true
+                }
+                
+                (NSApplication.shared().delegate as? AppDelegate)?.saveAction(nil)
+                textField.stringValue = ""
+                importantCheckbox.state = 0
+                
+                getToDoItems()
+            }
+        }
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return todoItems.count
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        if let cell = tableView.make(withIdentifier: "importantCell", owner: self) as? NSTableCellView {
+            cell.textField?.stringValue = "hello"
+            return cell;
+        }
+        return nil
+    }
+    
 
 }
 
